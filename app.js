bccab1f1493c0fdad206d2602df7b43032808860
cfalcant <cfalcant@gmail.com>
2017-12-08 15:03:34 -0700

    updated

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-$(document).ready(function() {
-  console.log("Hello!");
+// $(document).ready(function() {
 
+
+  let $xhr = $.getJSON('https://lit-fortress-6467.herokuapp.com/object');
+
+  $xhr.done(function(data) {
+    console.log(data)
+
+    for (var i = 0; i < data.length; i++) {
+      console.log(data.artist)
+  }
 });
+// $.load(function)
+// $.get("https://lit-fortress-6467.herokuapp.com/object" + $('#firstcover').val(), function(data) {
+//
+// }
+    // $("#firstcover").load("index.html"), function() {
+    //     $("#firstcover").attr("src", "./images/" + $(this).text() + ".jpg");
+    //     }
+    // });
